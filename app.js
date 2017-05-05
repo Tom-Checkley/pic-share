e4ceacbf0a06c773c6c7b2ba185997250fb118b2
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2017-05-05 13:45:49 +0100

    added path to index.html for unknown paths

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const express = require('express'),
-			path = require('path'),
-			bodyParser = require('body-parser'),
-			cors = require('cors'),
-			passport = require('passport'),
-			mongoose = require('mongoose'),
-			config = require('./config/database');
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  passport = require('passport'),
+  mongoose = require('mongoose'),
+  config = require('./config/database');
 
 /*
-*		DATABASE
-*/
+ *		DATABASE
+ */
 
 // Connect to MongoDB 
 mongoose.connect(config.database);
 
 // On Connection
 mongoose.connection.on('connected', () => {
-	console.log('Connected to database ' + config.database);
+  console.log('Connected to database ' + config.database);
 });
 
 // On Error
 mongoose.connection.on('error', (err) => {
-	console.log('Database error: ' + err);
+  console.log('Database error: ' + err);
 });
 
 
 /*
-*		APP
-*/
+ *		APP
+ */
 
 const app = express();
 
@@ -54,10 +54,15 @@ app.use('/users', users);
 
 // Index Route
 app.get('/', (req, res) => {
-	res.send('Invalid End Point');
+  res.send('Invalid End Point');
+});
+
+// catch incorrect paths
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start Server
 app.listen(3000, () => {
-	console.log('App listening on port ' + port);
+  console.log('App listening on port ' + port);
 });
